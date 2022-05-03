6b0fc6a5cd791b45d98a0a68a5052a71a58d3f93
VictoriaStarynchuk <91877111+VictoriaStarynchuk@users.noreply.github.com>
2022-05-03 17:54:13 +0300

    delete main

--- main/java/ua/lviv/iot/my_project/main/Main.java
+++ main/java/ua/lviv/iot/my_project/main/Main.java
@@ -9,56 +9,8 @@ import ua.lviv.iot.my_project.model.*;
 public class Main {
 
 	public static void main(String[] args) {
-
-//		ChildShopGoods TShirt = new Clothes("Clothes", "TSirts colorful", AgeCategory.ONETOFOURMONTH, "Zara", 399,
-//				Gender.Girls, ClothesType.TShirt, "pink");
-//		ChildShopGoods Gloves = new Clothes("Clothes", "Gloves for winter", AgeCategory.FOURWEEKS, "Mango", 200, Gender.UniSex,
-//				ClothesType.Gloves, "grey");
-//		ChildShopGoods BodySuit = new Clothes("Clothes", "BodySuit monotonne", AgeCategory.NINETOTWELFTHMONTH, "Teddy", 659,
-//				Gender.Boys, ClothesType.BodySuit, "blue");
-//
-//		ChildShopGoods Shampoo = new BathAndBodyCare("Bath And Body Care", "shampoo", AgeCategory.NINETOTWELFTHMONTH,
-//				"Bioderma", 89, Gender.UniSex, PackSize.Small);
-//
-//		ChildShopGoods Cereal = new FeedingCare("Feeding Care ", "Oats Cereal", AgeCategory.FIVETOEIGHTMONTH, "Milupa", 78,
-//				Gender.UniSex, "pumpkin, dry milk, no sugar", "25/06/2023");
-//
-//		ShopManager shopManager = new ShopManager(); 
-//		
-//		ArrayList<ChildShopGoods> goods = new ArrayList<ChildShopGoods>();
-//		goods.add(TShirt);
-//		goods.add(Gloves);
-//		goods.add(Shampoo);
-//		goods.add(Cereal);
-//		goods.add(BodySuit);
-//		System.out.println("Available goods: \n" + goods);
-//
-//		System.out.println("Goods sorted by price (ascending): \n ");
-//		shopManager.addGoods(goods);
-//		shopManager.sortByPriceAscending(goods);
-//		System.out.println(goods);
-//
-//		System.out.println("Goods sorted by price (descending): \n ");
-//		shopManager.sortByPriceDescending(goods);
-//		System.out.println(goods);
-//
-//		System.out.println("Goods sorted by name of product (ascending): \n ");
-//		shopManager.sortByNameAscending(goods);
-//		System.out.println(goods);
-
-//		System.out.println("Goods sorted by name of product (descending): \n ");
-//		shopManager.sortByNameDescending(goods);
-//		System.out.println(goods);
-//	
-//		List<ChildShopGoods> goodsSameAge = shopManager.findGoodsByAge(AgeCategory.NINETOTWELFTHMONTH);
-//		System.out.println("Goods for a certain age category:\n" + goodsSameAge);
 		
-		//writeCSV();
 	}
-		
-		//public static void writeCSV() throws IOException {
-	        //try (FileWriter writer = new FileWriter("result.csv")) {
-	           // writer.write("column 1, column 2, column 3\r\n");
-	        //} 
-	    }
+}
+
 
